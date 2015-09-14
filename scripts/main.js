3735b05460d5a1e3a0fd7a1635f8daa6492fcd65
Leslie Sutton <l4sutton@gmail.com>
2015-09-14 13:41:06 -0500

    pushing

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,14 @@
 'use strict';
+
+var $submitBtn = $('#submitBtn');
+
+$('#submitBtn').click(function() {
+	$('button').prop('disabled', true);
+	$submitBtn.html('Loading...');
+
+	setTimeout(function() {
+     $submitBtn.html('Submit');
+     $('button').prop('disabled', false);
+     }, 4000);
+	
+});
